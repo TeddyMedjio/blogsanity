b56445ff53be81cc789ae31836c763011cf23bfc
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2024-02-20 10:34:56 +0100

    hero section

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="flex flex-row px-5 md:px-10 lg:px-14 items-center justify-between mt-4">
       <div>
-        <Image
-          src="/img/logo.png"
-          height={50}
-          width={50}
-          alt="logo"
-          className="rounded-full"
-        />
+        <Link href="/">
+          <Image
+            src="/img/logo.png"
+            height={50}
+            width={50}
+            alt="logo"
+            className="rounded-full"
+          />
+        </Link>
       </div>
       <nav className=" flex flex-row space-x-4">
-        <p>Blog</p>
-        <p>About</p>
-        <p>Contact</p>
+        <Link href="/blog">Blog</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
       </nav>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "./components/Hero";
+
 export default function Home() {
   return (
-    <main className=" flex min-h-screen flex-col">
-      <h2 className="">home page</h2>
+    <main className=" flex min-h-screen flex-col px-5 md:px-10 lg:px-14">
+      <Hero />
     </main>
   );
 }
